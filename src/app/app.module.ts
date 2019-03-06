99253bb8e50b2f9279b6684f372f7324903bea40
Naushad Alama <naushad.jgec@gmail.com>
2019-03-06 12:03:52 +0530

    File choose and Upload in tab2

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//import { NavController, LoadingController, ToastController } from '@ionic/angular';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file';
+//Extra Libraries, Need to install
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    FileTransfer,
+    FileTransferObject,
+    FileChooser,
+    FilePath,
+    //NavController, LoadingController, ToastController
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,109 @@
+import { Component } from '@angular/core';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file';
+//Extra Libraries, Need to install
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  uri:any;
+  fileName:any;
+  uploadStatus:any;
+  uploadMessage:any;
+  loading:any;
+
+  constructor(public navCtrl: NavController,
+    private transfer: FileTransfer,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    private fileChooser: FileChooser,
+    private filePath: FilePath) {}
+
+  async uploadFile() {
+    //Choose the file using filechoose library
+    this.fileChooser.open()
+    .then(uri => {
+      this.uri=uri;
+      return uri;
+    })
+    //Get filename using filepath library
+    .then(uri=>this.filePath.resolveNativePath(uri))
+    .then(filePath => {
+      let filename:any = filePath.split('/');
+      filename = filename[filename.length - 1];
+      return(filename)
+    })
+    //Start Transfering using file transfer plugin
+    .then(filename=>{
+      this.fileName=filename;
+      this.loading = this.createLoading();
+      //this.dismissLoading(this.loading);
+      //this.presentLoading(loading);
+     /*  let loader = this.loadingCtrl.create({
+        message: "Uploading..."
+      });
+      loader.present(); */
+      
+      const fileTransfer: FileTransferObject = this.transfer.create();
+      let options: FileUploadOptions = {
+        fileKey: 'file',
+        fileName: filename,
+        chunkedMode: false,
+        headers: {}
+      }
+      fileTransfer.upload(this.uri, 'https://localhost/file/bill', options)
+        .then((data) => {
+          this.uploadStatus=data.responseCode;
+          this.uploadMessage=JSON.stringify(data.response);
+          console.log(data+" Uploaded Successfully");
+          alert(JSON.stringify(data.response));
+          //loader.dismiss();
+          //this.dismissLoading(this.loading);
+          this.presentToast("Image uploaded successfully");
+      }, (err) => {
+        console.log(err);
+        
+        //loader.dismiss();
+        //this.dismissLoading(this.loading);
+        this.presentToast(err);
+      });
+    })
+    .catch(err => {
+      //this.dismissLoading(this.loading);
+      alert(err)
+    });
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 6000,
+      position: 'bottom'
+    });
+
+    /* toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    }); */
+    //toast.present();
+  }
+  async createLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Uploading...',
+    });
+    return await loading.present();
+
+    //await loading.dismiss();
+
+    console.log('Loading dismissed!');
+  }
+
+  /* async dismissLoading(loading:LoadingController){
+    await loading.dismiss();
+  } */
+}
